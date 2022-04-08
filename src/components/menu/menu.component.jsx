a8feb17a281cfe994a432bc7a5b68eb26b08615b
jasonanthonblack@gmail.com <jasonanthonblack@gmail.com>
2022-04-07 19:27:05 -0500

    completed menu item component

--- src/components/menu/menu.component.jsx
+++ src/components/menu/menu.component.jsx
@@ -2,13 +2,46 @@ import { useState } from "react";
 import "./menu.styles.scss";
 
 const Menu = () => {
+  const [menuItemSelected, setMenuItemSelected] = useState("Recipe Search App");
+
+  const onClick = (event) => {
+    setMenuItemSelected(event.target.innerText);
+  };
+
   return (
     <section className="menu-container">
       <h1>Projects</h1>
       <menu className="menu">
-        <li className="menu-item">Recipe Search App</li>
-        <li className="menu-item">Shopping Cart</li>
-        <li className="menu-item">Brochure Landing Page</li>
+        <li
+          onClick={onClick}
+          className={
+            menuItemSelected === "Recipe Search App"
+              ? "menu-item active-bottom-border"
+              : "menu-item"
+          }
+        >
+          Recipe Search App
+        </li>
+        <li
+          onClick={onClick}
+          className={
+            menuItemSelected === "Shopping Cart"
+              ? "menu-item active-bottom-border"
+              : "menu-item"
+          }
+        >
+          Shopping Cart
+        </li>
+        <li
+          onClick={onClick}
+          className={
+            menuItemSelected === "Brochure Landing Page"
+              ? "menu-item active-bottom-border"
+              : "menu-item"
+          }
+        >
+          Brochure Landing Page
+        </li>
       </menu>
     </section>
   );
--- src/routes/projects/projects.component.jsx
+++ src/routes/projects/projects.component.jsx
@@ -1,7 +1,9 @@
-import "./projects.styles.scss";
+import { useState } from "react";
 
 import Menu from "../../components/menu/menu.component";
 import Footer from "../../components/footer/footer.component";
+
+import "./projects.styles.scss";
 
 const Projects = () => {
   return (
